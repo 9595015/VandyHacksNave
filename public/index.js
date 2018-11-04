b8a36f868b659d32a8dc42a71e8859828c3d795a
Gabriel Krivian <gabekrivian@gmail.com>
2018-11-04 02:31:56 -0500

    Update index.js

--- public/index.js
+++ public/index.js
@@ -1,27 +1,77 @@
-
 var currentStateVal = 0;
 var dateVal = 0;
+var apiString = 0;
+var month = 0;
+var day = 0;
+var longitudeStorLocations = [];
+var latitudeStorLocations = [];
+var marker = []
 $( document ).ready(function() {
     console.log( "ready!" );
     $('.calendar').datepicker();
     $('button').click(function(event) {
         dateVal = $('#calendar').val(); 
+        month = dateVal.slice(0,2);
+   	    day = dateVal.slice(3,5);
+        apiString = "s=" + currentStateVal + "&m=" + month + "&d=" + day;
     })
     $('.dropdown-menu a').on('click', function(){  
-        console.log($(this).data("value"));
         currentStateVal = $(this).data("value");
         $('.dropdown-toggle').html($(this).html());    
     })
-    $.getJSON("https://vandyhacks-1541273118069.firebaseapp.com/StoreLocator.json", function(json) {
-        console.log(json); // this will show the info it in firebug console
+    $.getJSON("StoreLocatore.json", function(json) {
+            console.log(json.length);
+            for(var i = 0; i < json.length; i++){
+                longitudeStorLocations.push(json[i].longitude);
+                latitudeStorLocations.push(json[i].latitude);
+            }
+            addMarkers();
     });
-//    console.log(csvJSON());
+    
+    
+window.onload = function () {
 
+var chart = new CanvasJS.Chart("chartContainer", {
+	animationEnabled: true,
+	theme: "light2", // "light1", "light2", "dark1", "dark2"
+	title:{
+		text: "Most Likely Purchased Items"
+	},
+	axisY: {
+		title: "% probability of Purchase"
+	},
+	data: [{        
+		type: "column",  
+
+		dataPoints: [      
+			{ y: 300878, label: "Venezuela" },
+			{ y: 266455,  label: "Saudi" },
+			{ y: 169709,  label: "Canada" },
+			{ y: 158400,  label: "Iran" },
+			{ y: 142503,  label: "Iraq" },
+		]
+	}]
+});
+chart.render();
+
+}
+    
 });
 var map;
 function initMap() {
-        map = new google.maps.Map(document.getElementById('map'), {
-          center: {lat: -34.397, lng: 150.644},
-          zoom: 8
-        });
+    map = new google.maps.Map(document.getElementById('map'), {
+        center: {lat: 39.83333333, lng: -98.58333333},
+        zoom: 5
+    });
+//    var place = {lat: 39.83333333, lng: -98.58333333};
+//    var marker = new google.maps.Marker({position: place, map: map});
 }
+function addMarkers(){
+    for(var i = 0; i < longitudeStorLocations.length; i++){
+        var place = {lat: latitudeStorLocations[i], lng: longitudeStorLocations[i]};
+        var marker = new google.maps.Marker({position: place, map: map});
+    }
+    var markerCluster = new MarkerClusterer(map, markers,
+            {imagePath: 'https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/m'});
+      
+      }
